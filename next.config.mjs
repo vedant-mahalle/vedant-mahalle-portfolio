4ed8028e3953b09aa4acd466e07feb3a75ed4841
Vedant Mahalle <vedantmahalle39@gmail.com>
2025-06-10 23:17:28 +0530

    First change

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/', // routes everything to the root
+      },
+    ];
+  },
 }
 
 export default nextConfig
